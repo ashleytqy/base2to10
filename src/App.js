99f79346b955148b6234f8f1555b0730e155bba0
Ashley Teoh <atq203@nyu.edu>
2017-05-11 19:21:32 -0400

    added feature to choose num of bits

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ class Display extends Component {
     this.state = {
       bits: [0, 0, 0, 0, 0, 0, 0, 0],
       total: 0,
+      sizes: [4, 8, 12, 16],
+      currentSize: 8
     }
 
     this.calculateTotal = this.calculateTotal.bind(this);
@@ -29,22 +31,46 @@ class Display extends Component {
     this.setState({bits: copy, total: this.calculateTotal(copy)});
   }
 
+  chooseSize(i) {
+    let newBits = [];
+    for (let x = 0; x < i; x++) {
+      newBits.push(0);
+    }
+    this.setState({currentSize: i, bits: newBits, total: 0});
+  }
+
   render() {
     const clickers = this.state.bits.map((count, i) => {
       return <Bit key={i} clickHandler={this.handleClick.bind(this, i)} bit={this.state.bits[i]}/>;
     });
 
+    let sizes = [];
+    for (let i = 0; i < this.state.currentSize; i++) {
+      const val = this.state.sizes[i];
+      sizes.push(<Size key={i} clickHandler={this.chooseSize.bind(this, val)} size={val}/>);
+    };
+
     return (
       <div className="binary">
         <h1 className="title">Binary to Decimal Calculator</h1>
+        <h2 className="words">how many # of bits would you like to experiment with?</h2>
+        {sizes}
         <h2 className="words">(click to switch the bits on and off)</h2>
         <div className="bits">
         {clickers}
         </div>
-        <div className="words total">total in base 10 is {this.state.total} </div>
+        <div className="words total">in base 10, this is {this.state.total} </div>
       </div>
     )
   }
+}
+
+class Size extends Component {
+ render() {
+    return (
+      <div className="bit" onClick={this.props.clickHandler}>{this.props.size}</div>
+    )
+  } 
 }
 
 class Bit extends Component {
